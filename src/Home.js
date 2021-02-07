e2c3628516c893b9fd6f152749181c3320328d26
kavindumadushanka972 <kavindumadushanka972@gmail.com>
2021-02-07 21:17:29 +0530

    delete blog option added

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,22 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-
-    const [name, setName] = useState('Mario')
-    const [age, setAge] = useState(21)
-
-    const handleClick = () =>{
-        setName('Kavindu')
-        setAge(23)
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+        {title: 'Welcome to party', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
+    ]);
+    
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id)
+        setBlogs(newBlogs)
     }
 
     return (  
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick = { handleClick } >Click Me</button>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!"/>
         </div>
     );
 }
